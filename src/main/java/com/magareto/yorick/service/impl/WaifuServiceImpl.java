a9277eaa468215d2f54542ee48e605b1c5ea8137
supernaucanera <nikolay.stanev.255@gmail.com>
2021-04-07 00:37:39 +0300

    Fixed typoo

--- src/main/java/com/magareto/yorick/service/impl/WaifuServiceImpl.java
+++ src/main/java/com/magareto/yorick/service/impl/WaifuServiceImpl.java
@@ -2,26 +2,18 @@ package com.magareto.yorick.service.impl;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.magareto.yorick.bot.command.utils.CommandUtils;
 import com.magareto.yorick.bot.constants.Constants;
 import com.magareto.yorick.bot.constants.ErrorMessages;
 import com.magareto.yorick.bot.exception.YorickException;
 import com.magareto.yorick.service.WaifuService;
-import discord4j.core.object.entity.channel.MessageChannel;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
-import org.apache.http.impl.nio.client.HttpAsyncClients;
-import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class WaifuServiceImpl implements WaifuService {
@@ -48,7 +40,7 @@ public class WaifuServiceImpl implements WaifuService {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
-    private final Set<String> nsfwGenre = new HashSet<>(Arrays.asList("waifu", "neko", "trap", "blowjob"));
+    private final Set<String> nsfwGenres = new HashSet<>(Arrays.asList("waifu", "neko", "trap", "blowjob"));
 
     private final String BASE_URL_FORMATTABLE = "http://waifu.pics/api/%s/%s";
     private static final String DEFAULT_TAG = "waifu";
@@ -66,7 +58,7 @@ public class WaifuServiceImpl implements WaifuService {
 
         if (tag == null) {
             tag = DEFAULT_TAG;
-        } else if ((!sfwGenres.contains(tag) && !nsfw) || (!nsfwGenre.contains(tag) && nsfw)) {
+        } else if ((!sfwGenres.contains(tag) && !nsfw) || (!nsfwGenres.contains(tag) && nsfw)) {
             throw new YorickException(ErrorMessages.INVALID_GENRE);
         }
 
