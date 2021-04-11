83331825a3ba90c65b84d73bef1655f963ccc3a3
supernaucanera <nikolay.stanev.255@gmail.com>
2021-04-12 00:28:09 +0300

    Changed constants

--- src/main/java/com/magareto/yorick/bot/constants/Constants.java
+++ src/main/java/com/magareto/yorick/bot/constants/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
     public static final String HELP_COMMAND = "help";
     public static final int STATUS_NOT_FOUND = 404;
 
-    public static final String DEV_ID = "165554929107206144";
+    public static final String DEV_ID = "1655549291072061444";
 }
